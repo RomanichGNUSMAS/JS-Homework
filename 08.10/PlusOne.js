1cd64db9509c626e7736033de9ce2efab3b06ce0
RomanichGNUSMAS <romanepremyan112009@gmail.com>
2025-10-17 16:46:31 +0400

    update

---
+++ 08.10/PlusOne.js
@@ -0,0 +1,14 @@
+var PlusOne = (digits) => {
+    for(let i = digits.length - 1; i >= 0; i--) {
+        if(digits[i] < 9) {
+            digits[i]++;
+            return digits;
+        }
+
+        digits[i] = 0;
+    }
+    digits.unshift(1);
+    return digits;
+}
+
+console.log(PlusOne([8,9,9,9,9]));
